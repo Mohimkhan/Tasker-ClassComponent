345601f2968dbfca3d7d4a0af9f19b303cc418ec
Mohimkhan <mohimkhan652@gmail.com>
2024-02-04 16:48:03 +0600

    Add Footer Component

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default class Home extends Component {
   render() {
     return (
       <>
         <NavBar />
+        <br />
+        <br />
+        <Footer />
       </>
     );
   }
